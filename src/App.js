21611219c2b3731de40e98fbf03e1aa64942ae16
AigerimAdr <aigerim210596@gmail.com>
2023-09-27 14:51:52 +0600

    сделала дз5,создала форму, вывела данные,добавила стили

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+export default function Form() {
+  const [PrototypeForm, setPrototypeForm] = useState({
+    key1: "Barbara",
+    key2: "Hepworth",
+    key3: "bhepworth@react.com",
+    key4: "love cats",
+  });
+
+  const formStyle = {
+    color: "blue",
+    backgroundColor: "lightgray",
+    padding: "15px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+  const inputStyle = {
+    margin: "5px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form style={formStyle}>
+      <label>
+        key1:
+        <input
+          style={inputStyle}
+          value={PrototypeForm.key1}
+          onChange={(event) => {
+            setPrototypeForm({
+              ...PrototypeForm,
+              key1: event.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        key2:
+        <input
+          style={inputStyle}
+          value={PrototypeForm.key2}
+          onChange={(event) => {
+            setPrototypeForm({
+              ...PrototypeForm,
+              key2: event.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        key3:
+        <input
+          style={inputStyle}
+          value={PrototypeForm.key3}
+          onChange={(event) => {
+            setPrototypeForm({
+              ...PrototypeForm,
+              key3: event.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        key4:
+        <input
+          style={inputStyle}
+          value={PrototypeForm.key4}
+          onChange={(event) => {
+            setPrototypeForm({
+              ...PrototypeForm,
+              key4: event.target.value,
+            });
+          }}
+        />
+      </label>
+      <p>
+        {PrototypeForm.key1} {PrototypeForm.key2} {PrototypeForm.key3}{" "}
+        {PrototypeForm.key4}
+      </p>
+    </form>
   );
 }
 
-export default App;
